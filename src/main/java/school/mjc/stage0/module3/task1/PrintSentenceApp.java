2ea16b6d70c00ee930f45391d4893374f57e9912
Нодир Асомов <nasomov93@gmail.com>
2023-10-14 20:56:58 +0500

    hi

--- src/main/java/school/mjc/stage0/module3/task1/PrintSentenceApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/PrintSentenceApp.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.module3.task1;
 
 public class PrintSentenceApp {
     public static void main(String[] args) {
-        System.out.println("java"+"\n"+ "is"+"\n"+"smth");
+        System.out.println( "This is my first Java program");;
 
 
     }
